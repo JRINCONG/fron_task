53948148c667c16913c514a40ca5e4093fa90cb3
Jairo Rincon <rincon303@gmail.com>
2024-09-22 10:28:02 -0500

    Edit de Items terminado

--- src/components/CardItems.jsx
+++ src/components/CardItems.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { getDestroyThunk } from '../store/slice/Items.slice'
+import React, { useEffect, useRef, useState } from 'react'
+import { getDestroyThunk, getEditThunk } from '../store/slice/Items.slice'
 import { useDispatch, useSelector } from 'react-redux'
+import { useForm } from 'react-hook-form'
 
 export const CardItems = ({item}) => {
 const dispatch= useDispatch()
+const {handleSubmit,reset, register} = useForm()
+const datos = useRef()
 const [IsValue, setIsValue]=useState(false)
 const [EditModal, setEditModal]=useState(false)
 const items= useSelector((store)=> store.ItemsSlice)
+
 const EditItems = ()=>{
  setEditModal(true)
 }
@@ -26,31 +30,35 @@ const RemoveItems= async() =>{
    setIsValue(true)
 
 }
-const EditaritemsUser=()=>{
+const EditaritemsUser=(data)=>{
   
+  dispatch(getEditThunk(item.id,data))
+ setEditModal()
 }
 
   
     
   return (
-    <div>
+    <>
       <div className='box_items'id={item.id}>
           <h2 >{item.description}</h2>
+          <div className='bts'>
           <button className='btn_delete' onClick={RemoveItems}><box-icon name='trash'></box-icon></button>
           <button className='btn_delete' onClick={EditItems}><box-icon name='edit'></box-icon></button>
-
+          </div>
         </div>       
         <div className={`Modal ${IsValue && 'Show'}`}>
           <h1>Items removed successfully👌</h1>
         </div>
         <div className={`Modal ${EditModal && 'vista'}`}>
-          <span className='cerrar'onClick={()=>{setEditModal(false)}}>x</span>
-          <input type='text' placeholder='Edit Items'/>
-          <button className='btn_vista' onClick={EditaritemsUser}><box-icon name='edit'></box-icon></button>
-
+          <span className='cerrar'onClick={()=>{setEditModal(false)}}>X</span>
+         <form  onSubmit={handleSubmit(EditaritemsUser)}>
+          <input {...register('description')}type='text' placeholder='Edit Items' name='description'/>
+          <button className='btn_vista'><box-icon name='edit'></box-icon></button>
+          </form>
         </div>
 
-    </div>
+    </>
   )
 }
 
--- src/store/slice/Items.slice.js
+++ src/store/slice/Items.slice.js
@@ -8,12 +8,13 @@ const Items= createSlice({
     reducers:{
         setShowItems:(state,action)=>state = action.payload,
         setAddItems:(state,{payload})=>[...state, payload],
-        setDestroyItems:(state,{payload})=>[...state].filter((item)=> item.id !== payload)
+        setDestroyItems:(state,{payload})=>[...state].filter((item)=> item.id !== payload),
+        setEditItems:(state,{payload})=>[...state].forEach((item)=>(item.id === payload.id ? item.description = payload.description: item))
         
     }
 })
 
-export const {setShowItems, setAddItems, setDestroyItems}=Items.actions
+export const {setShowItems, setAddItems, setDestroyItems,setEditItems}=Items.actions
 export default Items.reducer
 
 const ruta= import.meta.env.VITE_API_URL;
@@ -36,6 +37,12 @@ export const getDestroyThunk = (id) => (dispatch)=>{
   
         axios.delete(`${ruta}/items/${id}`,BearerToken())
        .then(resp=> dispatch(setDestroyItems(id)))
+       .catch(error => console.log(error))
+}
+
+export const getEditThunk =(id,data)=>(dispatch)=>{
+       axios.put(`${ruta}/items/${id}`,data, BearerToken())
+       .then(resp => dispatch(setEditItems(resp.data)))
        .catch(error => console.log(error))
 }
 
